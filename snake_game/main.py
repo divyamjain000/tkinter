a582fb292696b847d61bdf6a2542ad520d34b147
Divyam Jain <jaindivyam111@gmail.com>
2022-09-22 22:49:43 +0530

    initialized

--- snake_game/main.py
+++ snake_game/main.py
@@ -4,37 +4,39 @@ import random
 
 root=Tk()
 class canvas:
-    def __init__(self,parent):
-        super().__init__()
-        self.makeCanvas(parent,"black",400,400)
     
-    def quit_btn(self,parent):
-        btn=Button(parent,text="quit",bg="white",command=parent.quit)
+    crds={'0':(40,40)}
+    def __init__(self,parent):
+        makeCanvas(400,400)
+        self.parent=parent
+    
+    def quit_btn(self):
+        btn=Button(self.parent,text="quit",bg="white",command=self.parent.quit)
         btn.place(x=500,y=50)
 
-    def makeCanvas(self,parent,background,x,y):
-        parent.geometry("600x600")
-        canvas=Canvas(parent,height=x,width=y,bg=background)
+    def head(self):
+        x,y=50,50
+        bx=self.canvas.create_rectangle(x,y,x+20,y+20,fill="green")
+
+        self.mve(self.parent,canvas,bx)
+
+    def makeCanvas(self,x,y):
+        self.parent.geometry("600x600")
+        canvas=Canvas(self.parent,height=x,width=y,bg="black")
         canvas.place(x=100,y=100)
-        self.quit_btn(parent)
-        self.head(200,200,canvas,parent)
+        quit_btn()
+        head()
 
-    def move(self,parent,can_wid,bx):
-        can_wid.move(bx,20,0)
-        root.after(1000,self.move)
+    def mve(self,bx):
+        for i in range(3):
+            self.parent.after(1000, self.can_wid.move(bx,20,0))
+        
+        # root.after(1000,self.mve(parent,can_wid,bx))
 
-    def head(self,x,y,canvas,parent):
-        bx=canvas.create_rectangle(x,y,x+20,y+20,fill="green")
-        self.move(parent,canvas,bx)
     def body(self,x,y):
         bd=canvas.create_rectangle(x,y,x+20,y+20,fill="green")
         
-    def appendbody(self,a):
-        a=1
-        
-    
 
-    
 sketch=canvas(root)
 
 root.mainloop()
